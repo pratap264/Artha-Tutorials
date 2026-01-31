307a8bd820ce1f781aca1c694b6b7dc8742b863c
Pratap <pratapsinghagarud264@gmail.com>
2026-02-01 00:50:06 +0530

    removed extra files

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import React from 'react'; // Add this line
+import React from "react"; // Add this line
 import App from "./App.jsx";
 import "./index.css";
 import "slick-carousel/slick/slick.css";
@@ -9,8 +9,8 @@ import { BrowserRouter } from "react-router-dom";
 // Add the base path here
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/artha-tutorials">
+    <BrowserRouter basename="/artha-tutorials/">
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
